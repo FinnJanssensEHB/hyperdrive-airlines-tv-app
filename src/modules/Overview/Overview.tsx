35c85d3ee5fed35ce42bc0c08b9ade2ff9812579
FinnJanssensEHB <finn.janssens@student.ehb.be>
2022-03-30 14:09:24 +0200

    Style overview page

--- src/modules/Overview/Overview.tsx
+++ src/modules/Overview/Overview.tsx
@@ -4,6 +4,8 @@ import { fetchShows } from "../../Store/Shows/Facade";
 import { Show, ShowState } from "../../Store/Shows/InitialState";
 import { selectShows, selectShowsError } from "../../Store/Shows/Selectors";
 import { StoreState } from "../../Store/store.types";
+import OverviewItem from "./OverviewItem";
+import "./Overview.scss";
 
 function Overview() {
   const shows = useSelector<StoreState, Show[]>(selectShows);
@@ -16,11 +18,12 @@ function Overview() {
   console.log(shows);
   return (
     <>
-      <ul>
+      <div className="overview-container">
+        <h1 className="overview-title">TV-shows on today:</h1>
         {shows.map((show) => {
-          return <li>{show.name}</li>;
+          return <OverviewItem show={show}></OverviewItem>;
         })}
-      </ul>
+      </div>
     </>
   );
 }
--- src/modules/Overview/OverviewItem.tsx
+++ src/modules/Overview/OverviewItem.tsx
@@ -1,5 +1,20 @@
 import React, { useState } from "react";
+import { Show } from "../../Store/Shows/InitialState";
 
-function OverviewItem() {}
+import "./OverviewItem.scss";
+
+function OverviewItem(props: any) {
+  const show: Show = props.show;
+  return (
+    <>
+      <div className="overview-item">
+        <p className="item-showName">{show.showName}</p>
+        <p className="item-name">{show.name}</p>
+        <p className="item-type">{show.showType}</p>
+        <p className="item-airtime">{show.airtime}</p>
+      </div>
+    </>
+  );
+}
 
 export default OverviewItem;
