657de24ab69d08037d6fdc527a295a90a02dd5b0
FinnJanssensEHB <finn.janssens@student.ehb.be>
2022-03-31 10:18:29 +0200

    Add coloring of show type

--- src/modules/Overview/OverviewItem.tsx
+++ src/modules/Overview/OverviewItem.tsx
@@ -6,12 +6,39 @@ import "./OverviewItem.scss";
 
 function OverviewItem(props: any) {
   const show: Show = props.show;
+
+  function getTypeColor(type: string): string {
+    switch (type) {
+      case "Talk Show":
+        return "type-green";
+
+      case "Game Show":
+        return "type-magenta";
+      case "Scripted":
+        return "type-cyan";
+
+      case "Sports":
+        return "type-red";
+
+      case "Documentary":
+        return "type-white";
+
+      case "News":
+        return "type-yellow";
+
+      default:
+        return "type-blue";
+    }
+  }
+
   return (
     <>
       <Link to={"/show/" + show.showId} className="overview-item">
         <p className="item-showName">{show.showName}</p>
         <p className="item-name">{show.name}</p>
-        <p className="item-type">{show.showType}</p>
+        <p className={"item-type " + getTypeColor(show.showType)}>
+          {show.showType}
+        </p>
         <p className="item-airtime">{show.airtime}</p>
       </Link>
     </>
